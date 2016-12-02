c048f2fb208275d712405f05ebc06f1c4b5557b7
Tim Schaub <tim.schaub@gmail.com>
2016-12-02 11:27:00 -0800

    Expose bbox checking

--- test/index.test.js
+++ test/index.test.js
@@ -12,3 +12,33 @@ describe('xyzpdq()', () => {
   });
 
 });
+
+describe('containsBbox()', () => {
+
+  it('determines if a bbox is outside', () => {
+    const lookup = xyzpdq({
+      type: 'Point',
+      coordinates: [0, 0]
+    });
+    expect(lookup.containsBbox([-180, 45, -90, 90], 2)).toEqual(xyzpdq.NONE);
+  });
+
+  it('determines if a bbox partially filled', () => {
+    const lookup = xyzpdq({
+      type: 'Point',
+      coordinates: [0, 0]
+    });
+    expect(lookup.containsBbox([-180, -90, 180, 90], 2)).toEqual(xyzpdq.SOME);
+  });
+
+  it('determines if a bbox completely filled', () => {
+    const lookup = xyzpdq({
+      type: 'Polygon',
+      coordinates: [
+        [[-180, -90], [180, -90], [180, 90], [-180, 90], [-180, -90]]
+      ]
+    });
+    expect(lookup.containsBbox([-10, -10, 10, 10], 2)).toEqual(xyzpdq.ALL);
+  });
+
+});
